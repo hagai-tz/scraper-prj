30c5bd04bdad1ef5c55c2242be1fbb735a1caebf
hagai-tz <52886733+hagai-tz@users.noreply.github.com>
2019-12-17 12:18:03 +0200

    DB is up

---
+++ server/model/Article.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+// Document
+const ArticleSchema = new Schema({
+
+        vertical: String,
+        topic: String,
+        author: String,
+        content: Array,
+        date_published: Date,
+        domain: String,
+        discription: String,
+        lead_image_url: String,
+        title: String,
+        url: String,
+        word_count: Number
+})
+
+
+// Collection
+const Article = mongoose.model('article', ArticleSchema)
+module.exports = Article
