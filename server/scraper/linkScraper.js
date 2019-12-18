d7b087ad9a70438a1934c9cda6f1bea95d447400
Yoni <yonish3@gmail.com>
2019-12-18 17:38:37 +0200

    scraper 2.0

---
+++ server/scraper/linkScraper.js
@@ -0,0 +1,30 @@
+
+const $ = require('cheerio')
+const puppeteer = require('puppeteer');
+
+const articleUrlArray = []
+
+const linksScrapper = async function(sportSource){
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(sportSource.sportUrl, { waitUntil: 'networkidle2' })
+    const html = await page.content()
+
+    let articleURL = ""
+    const ilCount = $(sportSource.ilCountUrl, html).children().length
+
+    for (let index = 1; index < ilCount+1; index++) {
+        articleURL = $(sportSource.articleUrlStart + index + sportSource.articleUrlEnd, html).attr('href')   
+        articleUrlArray.push(articleURL)
+    }
+
+    await browser.close()
+    return articleUrlArray
+
+}
+
+
+//linksScrapper(sportSource[1])
+ 
+module.exports = linksScrapper 
---
+++ server/scraper/mainScraper.js
@@ -0,0 +1,39 @@
+const page = require('./pageScraper')
+const link = require('./linkScraper')
+
+const sportSource = [
+    {SourceName: 'NBC', 
+     sportUrl:"https://www.nbcsports.com/", 
+     ilCountUrl:"#block-bean-homepage-headline-stack > div > div > ul",
+     articleUrlStart:'#block-bean-homepage-headline-stack > div > div > ul > li:nth-child(',
+     articleUrlEnd: ') > a'
+    },
+    {SourceName: 'CNN', 
+     sportUrl:"https://edition.cnn.com/sport", 
+     ilCountUrl:"#sport-zone-1 > div.l-container > div.zn__containers > div:nth-child(1) > ul",
+     articleUrlStart:'#sport-zone-1 > div.l-container > div.zn__containers > div:nth-child(1) > ul > li:nth-child(',
+     articleUrlEnd: ') > article > div > div > h3 > a',
+     urlAddtoStart: 'https://edition.cnn.com/'
+     //needs to add 1 to CNN counter + add url part https://edition.cnn.com
+    }
+]
+
+articleObjArray = [];
+
+const mainScraper = async function(){
+    let obj 
+    let linkArray = await link(sportSource[1])
+    for (let index = 0; index < linkArray.length; index++) {
+        linkArray[index]='https://edition.cnn.com'+linkArray[index]
+    }
+
+    for (let index = 0; index < linkArray.length; index++) {
+        obj = await page(linkArray[index], 'cnn')
+        articleObjArray.push(obj)
+    }
+    
+    console.log(articleObjArray)
+
+}
+
+module.exports = mainScraper
