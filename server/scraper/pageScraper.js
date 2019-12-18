a978610a4aa87dcc8643ba16bc76ccf1ab1f2cd7
Yoni <yonish3@gmail.com>
2019-12-19 01:10:46 +0200

    small fix

--- server/scraper/pageScraper.js
+++ server/scraper/pageScraper.js
@@ -24,22 +24,7 @@ const pageScraper = async (articleUrl, magazine) => {
     
     if (magazine == 'cnn') {
       content = $('.zn-body__paragraph').text()
-    // let img = $('.l-container').find('img').attr('src')
-    // let img9 = $('.l-container').find('img').attr('data-src-medium')
-
-    imgSrc = $('.media__image.media__image--responsive').attr('data-src-medium')
-
-    //  let img2 = $('.l-container').find('img')
-    //  let img8 = $('.l-container')
-    //  let img5=img2[6].attribs.data-src-medium
-
-    //  let img3 = $('.l-container').children('img')
-    //  let img4 = $('.l-container').children('img').attr('src')
-    
- 
-    //  for (let index = 0; index < img2.length; index++) {
-     //   img2[index] = img2[index].attribs.data-src-medium
-    //  }
+      imgSrc = $('.media__image.media__image--responsive').attr('data-src-medium')
       topicTest = $("body > div.pg-right-rail-tall.pg-wrapper > article > div.l-container").text()
       topic = $("body > div.pg-right-rail-tall.pg-wrapper > article > div.l-container > h1").text()
       author = $("body > div.pg-right-rail-tall.pg-wrapper > article > div.l-container > div.metadata > div.metadata__info.js-byline-images > p.metadata__byline > span").text()
@@ -61,14 +46,12 @@ const pageScraper = async (articleUrl, magazine) => {
             content = bodyFull
           }
         }
-    
 
-          str = content
-          str = str.replace(/(^\s*)|(\s*$)/gi,"")
-          str = str.replace(/[ ]{2,}/gi," ")
-          str = str.replace(/\n /,"\n")
-          let word_count = str.split(' ').length
-          
+    str = content
+    str = str.replace(/(^\s*)|(\s*$)/gi,"")
+    str = str.replace(/[ ]{2,}/gi," ")
+    str = str.replace(/\n /,"\n")
+    let word_count = str.split(' ').length
 
     let articleObj = {
         vertical: 'sport',
