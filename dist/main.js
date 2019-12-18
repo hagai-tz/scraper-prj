fcaa7ffef20fb033d3acd629e526a8c668f583c7
Matanel Sitbon <matanelsitbon@gmail.com>
2019-12-18 16:45:13 +0200

    perfect ui

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,39 @@
 const articleManager = new ArticleManager();
 const renderer = new Renderer();
 
+$("#business").on("click", async function() {
+  let data = articleManager.getDataFromDB(business);
+  renderer.renderData(data);
+});
+
+$("#entertainment").on("click", async function() {
+  let data = articleManager.getDataFromDB(entertainment);
+  renderer.renderData(data);
+});
+
+$("#general").on("click", async function() {
+  let data = articleManager.getDataFromDB(general);
+  renderer.renderData(data);
+});
+
+$("#health").on("click", async function() {
+  let data = articleManager.getDataFromDB(health);
+  renderer.renderData(data);
+});
+
+$("#science").on("click", async function() {
+  let data = articleManager.getDataFromDB(science);
+  renderer.renderData(data);
+});
+
+$("#technology").on("click", async function() {
+  let data = articleManager.getDataFromDB(technology);
+  renderer.renderData(data);
+});
+
 $("#sports").on("click", async function() {
   let data = articleManager.getDataFromDB(sports);
   renderer.renderData(data);
 });
 
-/*$("document").ready(function() {
-  $("h1").fadeIn("slow");
-});*/
-
-$(document).ready(function() {
-  $(".sidenav").sidenav();
-});
+$("#card").on("click", async function() {});
